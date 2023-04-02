c9b1068a6a95844f0483eab956ad6618bb49d550
runuwuma <st097269@student.spbu.ru>
2023-04-02 21:49:38 +0300

    2

---
+++ main.py
@@ -0,0 +1,19 @@
+from aiogram import Bot, Dispatcher, types, executor
+from config import bot_token
+
+bot = Bot(bot_token)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.answer(text = message.text)
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.answer(text = message.text)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
+
