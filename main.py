0af08055be1282272e024a7122b2f7e7e198f765
runuwuma <st097269@student.spbu.ru>
2023-04-06 17:28:37 +0300

    Edition 9 main.py ed 5 conf

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 from aiogram import Bot, Dispatcher, types, executor
-from config import bot_token, help_commands, coaches, brief_comms
-from aiogram.types import ReplyKeyboardMarkup, ReplyKeyboardRemove, KeyboardButton
-
+from config import bot_token, help_commands, info_text
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 bot = Bot(bot_token)
 dp = Dispatcher(bot)
 
-keyboard_start = ReplyKeyboardMarkup(resize_keyboard=True)
-keyboard_start.add(KeyboardButton('/start')).add(KeyboardButton('/commands')).add(KeyboardButton('/coaches'))
+keyboard_start = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+keyboard_start.add(KeyboardButton('/start')).add(KeyboardButton('/commands')).add(KeyboardButton('/coaches'))   #coaches
 
+keyboard_info = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+keyboard_info.add(KeyboardButton('/contacts')).add(KeyboardButton('/atlant')).add(KeyboardButton('/location')).add(KeyboardButton('/news'))       #news, contacts, info
 
-@dp.message_handler(commands=['start'])   #старт
+@dp.message_handler(commands=['start'])
 async def start_command(message: types.Message):
     username = message.from_user.full_name
     answ1 = f'Здравствуйте, {username}!\n Это чат-бот детского спортивного клуба "Атлант" в Петергофе. Вы можете узнать все доступные команды в меню соощений или через команду /help.'
@@ -22,17 +23,16 @@ async def loc_command(message: types.Message):
     await message.answer_location(latitude=59.870235, longitude=29.846928)
     await message.answer('Мы находимся по адресу: Старый Петергоф, ул. Чичеринская, дом 2. Вход со стороны Гостилицкого шоссе, на цокольном этаже за подписью Amakids')
 
-@dp.message_handler(commands=['commands'])   #help
+@dp.message_handler(commands=['commands'])
 async def help_command(message: types.Message):
     await message.answer(text=help_commands, parse_mode='HTML')
     await message.delete()
 
-#список тренеров
-# @dp.message_handler(commands=['coaches', 'тренеры'])
-# async def help_command(message: types.Message):
-#     await message.answer(text=coaches, parse_mode='HTML')
-#     await message.delete()
+@dp.message_handler(commands=['info'])
+async def help_command(message: types.Message):
+    await message.answer(text=info_text, parse_mode='HTML', reply_markup=keyboard_info)
+    await message.delete()
+
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
-
