adfd228ed0b2749d40a29aa111eee874748de8a1
runuwuma <st097269@student.spbu.ru>
2023-04-06 21:40:55 +0300

    Edition 10 main.py ed 6 conf

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from aiogram import Bot, Dispatcher, types, executor
-from config import bot_token, help_commands, info_text
+from config import bot_token, help_commands, info_text, atlant_text
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 bot = Bot(bot_token)
@@ -9,12 +9,12 @@ keyboard_start = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=Tru
 keyboard_start.add(KeyboardButton('/start')).add(KeyboardButton('/commands')).add(KeyboardButton('/coaches'))   #coaches
 
 keyboard_info = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-keyboard_info.add(KeyboardButton('/contacts')).add(KeyboardButton('/atlant')).add(KeyboardButton('/location')).add(KeyboardButton('/news'))       #news, contacts, info
+keyboard_info.row(KeyboardButton('/contacts'), KeyboardButton('/atlant')).row(KeyboardButton('/location'))#, KeyboardButton('/back'))      #news, contacts
 
 @dp.message_handler(commands=['start'])
 async def start_command(message: types.Message):
     username = message.from_user.full_name
-    answ1 = f'Здравствуйте, {username}!\n Это чат-бот детского спортивного клуба "Атлант" в Петергофе. Вы можете узнать все доступные команды в меню соощений или через команду /help.'
+    answ1 = f'Здравствуйте, {username}!\nЭто чат-бот детского спортивного клуба "Атлант" в Петергофе. Вы можете узнать все доступные команды в меню соощений или через команду /commands.'
     await message.answer(answ1, reply_markup=keyboard_start)
     await message.delete()
 
@@ -31,6 +31,12 @@ async def help_command(message: types.Message):
 @dp.message_handler(commands=['info'])
 async def help_command(message: types.Message):
     await message.answer(text=info_text, parse_mode='HTML', reply_markup=keyboard_info)
+    await message.delete()
+
+
+@dp.message_handler(commands=['atlant'])
+async def help_command(message: types.Message):
+    await message.answer(text=atlant_text, parse_mode='HTML', reply_markup=keyboard_info)
     await message.delete()
 
 
