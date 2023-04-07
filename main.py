d31143d2e3620801f49871bdbfbd1011ccdb0913
runuwuma <st097269@student.spbu.ru>
2023-04-07 21:03:54 +0300

    Edition 11 main.py ed 7 conf

--- main.py
+++ main.py
@@ -1,22 +1,34 @@
 from aiogram import Bot, Dispatcher, types, executor
 from config import bot_token, help_commands, info_text, atlant_text
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton, InlineKeyboardMarkup
 
 bot = Bot(bot_token)
 dp = Dispatcher(bot)
 
-keyboard_start = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-keyboard_start.add(KeyboardButton('/start')).add(KeyboardButton('/commands')).add(KeyboardButton('/coaches'))   #coaches
+ikb_age = InlineKeyboardMarkup(row_width=2)
+ikb_age = InlineKeyboardButton(text='2.5 - 4', callback_data='')
 
-keyboard_info = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-keyboard_info.row(KeyboardButton('/contacts'), KeyboardButton('/atlant')).row(KeyboardButton('/location'))#, KeyboardButton('/back'))      #news, contacts
+info_ikb = InlineKeyboardMarkup()
+ib1 = InlineKeyboardButton(text='Наши контакты', callback_data='contacts' )
+ib2 = InlineKeyboardButton(text='Подробнее об Атланте', callback_data='atlant' )        #contacts
+ib3 = InlineKeyboardButton(text='Подробнее об Атланте', callback_data='location' )
+info_ikb.add(ib2)
+
+kb_info = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+kb_info.row(KeyboardButton('/contacts'), KeyboardButton('/atlant')).row(KeyboardButton('/location'))#, KeyboardButton('/back'))      #news, contacts
+
+start_ikb = InlineKeyboardMarkup()
+sb2 = InlineKeyboardButton(text='Вывести список команд', callback_data='comms' )
+start_ikb.add(sb2)
 
 @dp.message_handler(commands=['start'])
 async def start_command(message: types.Message):
     username = message.from_user.full_name
-    answ1 = f'Здравствуйте, {username}!\nЭто чат-бот детского спортивного клуба "Атлант" в Петергофе. Вы можете узнать все доступные команды в меню соощений или через команду /commands.'
-    await message.answer(answ1, reply_markup=keyboard_start)
+    answ1 = f'Здравствуйте, {username}!\nЭто чат-бот детского спортивного клуба "Атлант" в Петергофе.\
+    Вы можете узнать все доступные команды с помощью кнопки ниже или через команду /commands.\n'
+    await message.answer(answ1, reply_markup=start_ikb, parse_mode='html')
     await message.delete()
+
 
 @dp.message_handler(commands=['location'])
 async def loc_command(message: types.Message):
@@ -30,13 +42,13 @@ async def help_command(message: types.Message):
 
 @dp.message_handler(commands=['info'])
 async def help_command(message: types.Message):
-    await message.answer(text=info_text, parse_mode='HTML', reply_markup=keyboard_info)
+    await message.answer(text=info_text, parse_mode='HTML', reply_markup=kb_info)
     await message.delete()
 
 
 @dp.message_handler(commands=['atlant'])
 async def help_command(message: types.Message):
-    await message.answer(text=atlant_text, parse_mode='HTML', reply_markup=keyboard_info)
+    await message.answer(text=atlant_text, parse_mode='HTML', reply_markup=ikb_age)
     await message.delete()
 
 
